19de15c460461e88908071e2e0bb3ce8b59c5a67
ShrutiNambivengadam <73392190+ShrutiNambivengadam@users.noreply.github.com>
2021-02-08 19:00:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database;
+var form, game, player;
+var gameState = 0
+var playerCount;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
+
